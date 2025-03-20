7849e2c617b0a24333320703b891ad29f19e8da8
hardik47278 <hardikanand47h@gmail.com>
2025-03-20 11:07:39 +0530

    flask app created

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,request,make_response
+import os
+from flask_cors import CORS,cross_origin
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Web app with python flask"
+
+
+if __name__ =='__main__':
+    app.run(host='127.0.0.1',port=5000)
+
+
+
